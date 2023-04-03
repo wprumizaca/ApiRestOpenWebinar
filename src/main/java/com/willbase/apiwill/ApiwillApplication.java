5e6c872ffda51495cfb867a2acb6d58be020dcad
wprumizaca <wperezrumizaca@gmail.com>
2023-04-03 18:07:25 +0200

    Estado inicial del proyecto

---
+++ src/main/java/com/willbase/apiwill/ApiwillApplication.java
@@ -0,0 +1,13 @@
+package com.willbase.apiwill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiwillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiwillApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/willbase/apiwill/ApiwillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.willbase.apiwill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiwillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
